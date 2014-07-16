52658a57ba0e76379a6cb1b034d528168a1e3552
felixdae <felixdae@gmail.com>
2014-07-16 15:32:30 +0800

    test carriage return

---
+++ test_cr.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    for (char c='a';c<='z';c++)
+    {   
+        for (int i=0; i<10; ++i)
+        {   
+            printf("%c", c); 
+            fflush(stdout);
+            usleep(100*1000);
+        }   
+        printf("\r");
+    }   
+    return 0;
+}
+
