71a71055de4d9513143244dc8ec3bd9d115d241c
felixdae <felixdae@gmail.com>
2015-08-28 00:05:10 +0800

    coprime

--- haskell/99-problems/p32.hs
+++ haskell/99-problems/p32.hs
@@ -1,12 +1,5 @@
 -- import Debug.Trace (trace)
-
-myGcd x y
-    |x < 0 = myGcd (abs x) y
-    |y < 0 = myGcd x (abs y)
-    |y < x = myGcd y x
-    |x == 0 = y
-    -- |otherwise = trace (show x ++ ":" ++ show y) myGcd (mod y x) x
-    |otherwise = myGcd (mod y x) x
+import Gcd
    
 
 main = do
