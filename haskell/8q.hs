29cdf2b633c7e3016132b5e2f03ac3abc1ab68b7
felixdae <felixdae@gmail.com>
2015-08-27 23:45:15 +0800

    solve 8q

--- haskell/8q.hs
+++ haskell/8q.hs
@@ -1,9 +1,25 @@
-import Data.List
+import Data.List (permutations)
+-- import Debug.Trace (trace)
+sameRow (ar, ac) (br, bc) = ar /= br
+sameDiag (ar, ac) (br, bc) = (ar - ac) /= (br - bc)
+sameRDiag (ar, ac) (br, bc) = (ar + ac) /= (br + bc)
 
-eightQueen
+check cmp a b = cmp a b
 
-appendOne okSet
+checkAgainst _ _ _ [] = True
+-- checkAgainst i x j (y:ys) = (sameRow (x, i) (y, j))
+--                             && (sameDiag (x, i) (y, j))
+checkAgainst i x j (y:ys) = (sameDiag (x, i) (y, j))
+                            && (sameRDiag (x, i) (y, j))
+                            && (checkAgainst i x (j+1) ys)
 
-findPos okSet
-    | length okSet == 8 = okSet
-    | otherwise = findPos (appendOne okSet)
+gCheck _ [] = True
+gCheck i (x:xs) = (checkAgainst i x (i+1) xs) && (gCheck (i + 1) xs)
+
+-- eightQueen = [x | x <- [ [r1,r2,r3,r4,r5,r6,r7,r8] | r1 <- [1..8], r2 <- [1..8], r3 <- [1..8], r4 <- [1..8],
+--                                         r5 <- [1..8], r6 <- [1..8], r7 <- [1..8], r8 <- [1..8]], trace (show x) gCheck 1 x]
+eightQueen = [x | x <- permutations [1,2,3,4,5,6,7,8], {-trace (show x)-} gCheck 1 x]
+
+main = do
+    print $ length eightQueen
+    print eightQueen
