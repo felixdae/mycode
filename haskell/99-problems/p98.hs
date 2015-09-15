816c8337e771434bce317ae62faa280e4b501e8d
felixdae <felixdae@gmail.com>
2015-09-15 17:54:38 +0800

    nonogram solved

--- haskell/99-problems/p98.hs
+++ haskell/99-problems/p98.hs
@@ -2,6 +2,7 @@ import System.Environment
 import Text.Parsec
 import Text.Parsec.String
 import Control.Monad
+import Data.List
 
 regularParse :: Parser a -> String -> Either ParseError a
 regularParse p = parse p ""
@@ -18,11 +19,12 @@ nonEmptyListOfInt = do
     void $ whitespace
     lexeme $ char '['
     first <- lexeme $ num
-    rest <- lexeme $ many (do 
-                            void $ lexeme $ (char ',')
-                            i <- (lexeme $ num)
-                            return i
-                            )
+    rest <- lexeme $ many $ (void $ lexeme (char ',')) >> lexeme num
+    -- rest <- lexeme $ many (do 
+    --                         void $ lexeme $ (char ',')
+    --                         i <- (lexeme $ num)
+    --                         return i
+    --                         )
     lexeme $ char ']'
     return $ first:rest
     
@@ -42,11 +44,12 @@ nonEmptyListOfListOfInt = do
     void $ whitespace
     lexeme $ char '['
     first <- lexeme $ listOfInt
-    rest <- lexeme $ many $ (do
-                                void $ lexeme $ (char ',')
-                                li <- lexeme $ listOfInt
-                                return li
-                                )
+    -- rest <- lexeme $ many $ (do
+    --                             void $ lexeme $ (char ',')
+    --                             li <- lexeme $ listOfInt
+    --                             return li
+    --                             )
+    rest <- lexeme $ many $ (void $ lexeme (char ',')) >> lexeme listOfInt
     lexeme $ char ']'
     return $ first:rest
 
@@ -62,16 +65,70 @@ parseListOfList parser str = case regularParse parser str of
     Left _ -> []
     Right l -> l
 
-getRowDist nc row = 
+makePsb 1 s = [[s]]
+makePsb n s = concatMap (\x -> [x:y | y <- makePsb (n-1) (s-x)]) [0..s]
 
-calcNonoGram nr nc fistRowDist colDes = 
+genRcPsbLayout _ [] = []
+genRcPsbLayout s ln
+    |(sum ln) + (length ln) - 1 > s = []
+    |otherwise = map (makeAllRcLayout 0 ln) (makePsb (length ln + 1) (s - ((sum ln) + (length ln) - 1)))
+        where
+        makeAllRcLayout _ [] [] = []
+        makeAllRcLayout start [ys] [ns] = (map (\x -> (start + x, 1)) [0..(ys-1)]) ++ makeAllRcLayout (start + ys) [] [ns]
+        makeAllRcLayout start ln psb
+            |length psb > length ln = (map (\x -> (start + x, 0)) [0..(head psb-1)]) ++ makeAllRcLayout (start + (head psb)) ln (tail psb)
+            |otherwise = (map (\x -> (start + x, 1)) [0..(head ln-1)]) ++ [((start + head ln),0)] ++ makeAllRcLayout (start + 1 + (head ln)) (tail ln) psb
 
-solveNonoGram rowDes ColDes = calcNonoGram nr nc rowDist colDes
+
+checkLayout rcl [] = False
+checkLayout rcl (psb:rcPsb)
+    |harmony rcl psb = True
+    |otherwise = checkLayout rcl rcPsb
+        where
+        harmony [] p = True
+        harmony (x:xs) p
+            |elem x p = harmony xs p
+            |otherwise = False
+        -- harmony l p = foldr (`elem` p) True l
+
+collectRow r layout = map (\((_,y), n) -> (y,n)) (filter (\((x,_), _) -> x==r) layout)
+collectCol c layout = map (\((x,_), n) -> (x,n)) (filter (\((_,y), _) -> y==c) layout)
+
+cst l = map sum (filter (\x -> head x == 1) (group (map (\(_, x) -> x) l)))
+testGenRcPsbLayout ori s res = map (\x -> cst x == ori && s == length x && tpos x) res
+tpos l = map (\(x,_)->x) l == [0..length l - 1]
+
+solveNonogram rowCons colCons = trySolve 0 []
+    where
+    rn = length rowCons
+    cn = length colCons
+    allRowPsb = map (\x -> genRcPsbLayout cn x) rowCons
+    allColPsb = map (\x -> genRcPsbLayout rn x) colCons
+    trySolve n layout
+        |n >= rn*cn = [layout]
+        |otherwise = tryPlace (div n cn, mod n cn) 0 n layout ++ tryPlace (div n cn, mod n cn) 1 n layout
+    tryPlace (x,y) chi n layout
+        |checkLayout (collectRow x (((x,y),chi):layout)) (getRowPsb x) && checkLayout (collectCol y (((x,y),chi):layout)) (getColPsb y) = trySolve (n+1) (((x,y),chi):layout)
+        |otherwise = []
+    getRowPsb r = allRowPsb !! r
+    getColPsb c = allColPsb !! c
+
+-- collectRes :: [[((Int,Int),Int)]]->String
+collectRes r c = concatMap ((++"\n") . collectOneRes 0 r c)
+
+-- collectOneRes :: Int->[((Int,Int),Int)]->String
+collectOneRes n r c layout
+    |n >= r*c = ""
+    |otherwise = show (head [z | ((x,y),z) <- layout, (c*x+y)==n]) ++ (if mod (n+1) c == 0 then "\n" else " ")++ collectOneRes (n+1) r c layout
 
 main = do
+    -- print $ length (makePsb 3 8)
+    -- print $ genRcPsbLayout 10 [2,3,1]
+    -- print $ testGenRcPsbLayout [2,3,1] 10 (genRcPsbLayout 10 [2,3,1])
     -- x <- getArgs
     -- print x
     [rowDes,colDes] <- getArgs
-    print $ parseListOfList listOfListOfInt rowDes
-    print $ parseListOfList listOfListOfInt colDes
+    let rd = parseListOfList listOfListOfInt rowDes
+    let cd = parseListOfList listOfListOfInt colDes
+    putStr $ collectRes (length rd) (length cd) (solveNonogram rd cd)
     -- print $ regularParse num "12344"
