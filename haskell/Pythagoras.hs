e27b2738986065dd907c2a933fc3d8431fe9cab9
felixdae <felixdae@gmail.com>
2015-10-25 01:17:51 +0800

    Pythagoras with sbv

---
+++ haskell/Pythagoras.hs
@@ -0,0 +1,32 @@
+import Data.SBV
+
+gg :: Symbolic SBool
+gg = do
+    x <- exists "x"
+    y <- exists "y"
+    z <- exists "z"
+    constrain $ x .> 0
+    constrain $ y .> 0
+    constrain $ z .> 0
+    constrain $ x .< 100
+    constrain $ y .< 100
+    constrain $ z .< 100
+    constrain $ x*x + y*y .== z*z
+    -- return $ x*x*x + y*y*y .== z*z*(z::SWord16)
+    return $ x*x + y*y .== z*(z::SWord16)
+
+main = do
+    -- lr <- allSatWith z3{timing=True} gg
+    lr <- allSat gg
+    printRes lr
+        where
+        printRes (AllSatResult (br, ls)) = do
+            -- print br
+            mapM_ (display . extractModel) ls
+-- main = extractModel `fmap` allSatWith z3{timing=True} gg >>= display
+--     where
+            where
+            display :: Maybe (Word16, Word16, Word16) -> IO ()
+            display Nothing = putStrLn "No solution."
+            display (Just x) = print x
+ 
