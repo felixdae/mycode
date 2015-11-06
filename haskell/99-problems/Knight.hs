f0e70ce184ecb9dd6abbbe97f4a3b2591874ec5d
felixdae <felixdae@gmail.com>
2015-11-06 11:55:37 +0800

    optimize for n*m board

--- haskell/99-problems/Knight.hs
+++ haskell/99-problems/Knight.hs
@@ -1,10 +1,11 @@
 module Knight
 (Path(..),
-Dsf,
+Dsf(..),
+BoardPath(..),
 Pos,
 moveKnight,
 inMany,
-tryMany,
+-- tryMany,
 nextPos
 )
 where
@@ -26,6 +27,12 @@ inMany x start = return start >>= foldr (<=<) return (replicate x moveKnight)
 
 type Pos = (Int, Int)
 type Dsf = [Pos]
+
+data BoardPath = BoardPath {
+                getRow::Int,
+                getCol::Int,
+                getPath::[Pos]
+                }
 data Path a = Path a deriving (Show)
 
 instance Functor Path where
@@ -49,9 +56,9 @@ move (x, y) 6 = (x - 2, y - 1)
 move (x, y) 7 = (x - 2, y + 1)
 move (x, y) 8 = (x - 1, y + 2)
 
-legalMove :: Dsf -> Pos -> Bool
-legalMove p (x, y)
-    |not (x `elem` [1..8] && y `elem` [1..8]) = False
+legalMove :: Dsf -> Int -> Int -> Pos -> Bool
+legalMove p row col (x, y)
+    |not (x `elem` [1..row] && y `elem` [1..col]) = False
     |(x, y) `elem` p = False
     |otherwise = True
 
@@ -86,45 +93,58 @@ gd f t
 --     where
 --         np = makeMove path 0
 
-chooseMin :: Dsf -> [Pos] -> Maybe Pos
-chooseMin _ [] = Nothing
-chooseMin path@(p:ps) candis = Just (foldr (\acc c -> (choice c acc)) (head candis) candis)
+chooseMin :: Dsf -> [Pos] -> Int -> Int -> Maybe Pos
+chooseMin _ [] _ _ = Nothing
+chooseMin path@(p:ps) candis row col = Just (foldr (\acc c -> (choice c acc)) (head candis) candis)
     where
         choice a b
             |la < lb = a
             |la > lb = b
             |otherwise = if (gd p a) < (gd p b) then a else b
                 where
-                    la = length (filter (legalMove path) [move a x | x<-[1..8]])
-                    lb = length (filter (legalMove path) [move b x | x<-[1..8]])
+                    la = length (filter (legalMove path row col) [move a x | x<-[1..8]])
+                    lb = length (filter (legalMove path row col) [move b x | x<-[1..8]])
 
 
-makeMove :: Dsf -> Int -> Maybe Pos
-makeMove p@(h:hs) s = chooseMin p (filter (legalMove p) [move h x | x<-[1..8]])
+makeMove :: Dsf -> Int -> Int -> Int -> Maybe Pos
+makeMove p@(h:hs) s row col = chooseMin p (filter (legalMove p row col) [move h x | x<-[1..8]]) row col
 
 -- makeMove p@(h:hs) s = foldl (\acc d -> case acc of
 --                         (Just _) -> acc
 --                         Nothing -> if legalMove p (move h d) then Just (move h d) else acc) Nothing [s+1..8]
 
-back :: Dsf -> Dsf
-back path@(p:pn:ps) = 
+back :: Dsf -> Int -> Int -> Dsf
+back path@(p:pn:ps) row col = 
     -- |np /= pn = {-trace (show np)-} (np:pn:ps)
     -- |otherwise = {-trace "back"-} back (pn:ps)
     case mnp of
         (Just np) -> {-trace (show np)-} (np:pn:ps)
-        Nothing -> {-trace "back"-} back (pn:ps)
+        Nothing -> {-trace "back"-} back (pn:ps) row col
     where
-        mnp = makeMove (pn:ps) (gd pn p)
+        mnp = makeMove (pn:ps) (gd pn p) row col
 
-nextPos :: Dsf -> Path Dsf
-nextPos path@(p:ps) = 
+-- nextPos :: Dsf -> Path Dsf
+-- nextPos path@(p:ps) = 
+--     -- |np /= p = {-trace (show np)-} Path (np:path)
+--     -- |otherwise = {-trace "back"-} return $ back path
+--     case mnp of
+--         (Just np) -> {-trace (show np)-} Path (np:path)
+--         Nothing -> {-trace "back"-} return $ back path
+--     where
+--         mnp = makeMove path 0
+
+nextPos :: BoardPath -> Path BoardPath
+nextPos bp =
     -- |np /= p = {-trace (show np)-} Path (np:path)
     -- |otherwise = {-trace "back"-} return $ back path
     case mnp of
-        (Just np) -> {-trace (show np)-} Path (np:path)
-        Nothing -> {-trace "back"-} return $ back path
+        (Just np) -> {-trace (show np)-} Path (BoardPath row col (np:path))
+        Nothing -> {-trace "back"-} return $ BoardPath row col (back path row col)
     where
-        mnp = makeMove path 0
+        mnp = makeMove path 0 row col
+        row = getRow bp
+        col = getCol bp
+        path@(p:ps) = getPath bp
 
-tryMany :: Int -> Pos -> Path Dsf
-tryMany x start = return [start] >>= foldr (<=<) return (replicate x nextPos)
+-- tryMany :: Int -> Pos -> Path Dsf
+-- tryMany x start = return [start] >>= foldr (<=<) return (replicate x nextPos)
--- haskell/99-problems/p91.hs
+++ haskell/99-problems/p91.hs
@@ -2,11 +2,12 @@ import Knight
 import Data.List
 import Debug.Trace (trace)
 
-knightTravel :: Pos -> Dsf -> Path Dsf
-knightTravel s pf = do
-    path <- (return pf >>= nextPos)
+knightTravel :: Pos -> Dsf -> Int -> Int -> Path BoardPath
+knightTravel s pf row col = do
+    bp <- (return (BoardPath row col pf) >>= nextPos)
+    let path = getPath bp
     -- if {- trace (show (head path)) -} head path == s then return path else knightTravel s path
-    if length path == 64 then return path else knightTravel s path
+    if length path == row*col then return (BoardPath row col path) else knightTravel s path row col
 
 check :: Dsf -> Bool
 check [_] = True
@@ -15,10 +16,12 @@ check ((x,y):(a,b):qs)
     |otherwise = check ((a,b):qs)
 
 main = do
-    case knightTravel (1,1) [(1,1)] of
-        Path path -> do
-            print $ all (\(x,y) -> elem x [1..8] && elem y [1..8]) path
-            print $ all (\x -> length x == 1) (group path)
-            print $ check path
-            print . reverse $ path
+    let (Path bp) = knightTravel (1,1) [(1,1)] 24 24
+    -- case (getPath bp) of
+    --     Path path -> do
+    let path = getPath bp
+    print $ all (\(x,y) -> elem x [1..8] && elem y [1..8]) path
+    print $ all (\x -> length x == 1) (group path)
+    print $ check path
+    print . reverse $ path
     
